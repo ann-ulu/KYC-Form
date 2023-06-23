969c9cc13c4d541bdec59d8573a596b6c873bc81
Luchioffice <uluann1@gmail.com>
2023-06-23 11:30:12 +0100

    the commit

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+
+const picElement = document.querySelectorAll('.best');
+picElement.forEach((el) => observer.observe(el));
